b7dbd6d817a9b186d007df829dac5ea0563d5826
rike06 <104802906+rike06@users.noreply.github.com>
2022-05-30 21:52:06 -0300

    Add files via upload

---
+++ funcoes_js.js
@@ -0,0 +1,13 @@
+var nome = 'aluno' // string
+var idade = 27     //int
+var semdefinicao   //underfined
+var nula = null    // objeto(!?)
+
+const nota = 0             //int
+const sobenome = 'zaneti'  //string
+const naodefinida          // underfined
+
+
+
+
+
