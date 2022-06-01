d7b00b31bb17ad09d4d06193be15afb0bb484d99
rike06 <104802906+rike06@users.noreply.github.com>
2022-05-31 21:55:38 -0300

    Add files via upload

---
+++ testes_js/teste.js
@@ -0,0 +1,7 @@
+let data_hora = new Date();
+let hora = data_hora.getHours();
+if (hora < 12){
+   console.log ('Bom dia');
+ } else{
+    console.log("óla,tenha um Bom dia");
+ }
---
+++ testes_js/teste4.js
@@ -0,0 +1,11 @@
+let limite = 100;
+let saldo_atual = 20;
+
+let resultado = (saldo_atual < limite) ? "Necessito depositar $" : "Não necessito Depósito";
+console.log(r)
+
+/*
+
+sintaxe:
+let variável = (condição) ? //instrução se true : //instrução se falso
+*/
